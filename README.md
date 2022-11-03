625810d342c3366839d558122d20ddd235084cee
Ivan Nieto <ivan.nieto@kavak.com>
2022-11-02 23:34:11 -0600

    feat: add readme + splash screen

---
+++ README.md
@@ -0,0 +1,10 @@
+# Practica2
+
+![Github licence](http://img.shields.io/badge/license-MIT-blue.svg)
+
+## Descripción
+Esta aplicación fue creada para realizar una practica 2 de la asignatura de Desarrollo de Aplicaciones Web.
+La aplicación realiza operaciones CRUD sobre una base de datos SQLite, se centra sobre el giro automotriz.
+
+## License
+This project is license under MIT license.
---
+++ app/src/main/java/com/example/practica2/view/activities/Splash.kt
@@ -0,0 +1,21 @@
+package com.example.practica2.view.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.practica2.R
+import kotlin.concurrent.thread
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        thread{
+            Thread.sleep(3000)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
